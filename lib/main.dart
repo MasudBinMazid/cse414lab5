8e86e141d9c43d8642bdcbab06584a2fa2ae98c0
MasudBinMazid <mamun15-5451@diu.edu.bd>
2025-02-19 10:39:35 +0600

    task2

--- lib/main.dart
+++ lib/main.dart
@@ -10,43 +10,39 @@ class MyWidget extends StatelessWidget {
     return MaterialApp(
       title: "Basic UI-Flutter",
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Basic Flutter"),
-            backgroundColor: const Color.fromARGB(255, 178, 3, 62),
-            foregroundColor: const Color.fromARGB(255, 196, 208, 26),
-            centerTitle: true,
-            elevation: 20.5,
-            shadowColor: Colors.blue,
-          ),
-          body: Row(
+        appBar: AppBar(
+          title: Text("Basic Flutter"),
+          backgroundColor: const Color.fromARGB(255, 178, 3, 62),
+          foregroundColor: const Color.fromARGB(255, 196, 208, 26),
+          centerTitle: true,
+          elevation: 20.5,
+          shadowColor: Colors.blue,
+        ),
+        body: Container(
+          height: double.infinity,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Icon(
                 Icons.star,
                 size: 60,
                 color: const Color.fromARGB(255, 214, 228, 16),
               ),
-               Icon(
+              Image(
+                image: AssetImage('assets/shoe.png'),
+                width: 100,
+                height: 100,
+              ),
+              Icon(
                 Icons.star,
                 size: 60,
                 color: const Color.fromARGB(255, 214, 228, 16),
               ),
-                Icon(
-                  Icons.star,
-                  size: 60,
-                  color: const Color.fromARGB(255, 180, 22, 22),
-                ),
-                Icon(
-                  Icons.star,
-                  size: 60,
-                  color: const Color.fromARGB(255, 173, 20, 20),
-                ),
-                Icon(
-                  Icons.star,
-                  size: 60,
-                  color: const Color.fromARGB(255, 190, 25, 25),
-                ),
             ],
-          )),
+          ),
+        ),
+      ),
     );
   }
 }

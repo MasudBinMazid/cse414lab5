4cb63bc9438bedb4920aa8410eb2b6260aeec1ad
MasudBinMazid <mamun15-5451@diu.edu.bd>
2025-02-19 12:41:20 +0600

    banner_design

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,114 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyWidget());
+void main() {
+  runApp(const MyApp());
+}
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Basic UI-Flutter",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Basic Flutter"),
-          backgroundColor: const Color.fromARGB(255, 178, 3, 62),
-          foregroundColor: const Color.fromARGB(255, 196, 208, 26),
-          centerTitle: true,
-          elevation: 20.5,
-          shadowColor: Colors.blue,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        title: Text('Welcome to Flutter'),
+        titleTextStyle: TextStyle(
+            fontWeight: FontWeight.bold, color: Colors.white, fontSize: 30),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(30),
+        margin: EdgeInsets.only(left: 50, right: 50, top: 100, bottom: 100),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: const Color.fromARGB(255, 242, 235, 235),
         ),
-        body: Container(
-          //height: double.infinity,
-          width: double.infinity,
-          child: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: List.generate(
-                100,(index)=>Icon(
-                  Icons.star,
-                  size: index.toDouble(),
-                  color: Colors.blue,
-                )
-                  
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Text(
+                "50% OFF",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
                 ),
-           
+              ),
+              Icon(Icons.heart_broken, color: Colors.red),
+            ]),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    image: DecorationImage(
+                      image: AssetImage('assets/shoe.png'),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    "Apple Watch - series 6",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ]),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Text(
+                "\$ 45,000",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "\$ 55,000",
+                style: TextStyle(
+                  fontSize: 20,
+                  decoration: TextDecoration.lineThrough,
+                  color: Colors.grey,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ]),
+          ],
         ),
       ),
     );

b1780e3ba1551a4747e7ebd95d85d1fe1549caf7
MasudBinMazid <mamun15-5451@diu.edu.bd>
2025-02-19 09:56:21 +0600

    task1

--- lib/main.dart
+++ lib/main.dart
@@ -10,31 +10,43 @@ class MyWidget extends StatelessWidget {
     return MaterialApp(
       title: "Basic UI-Flutter",
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Basic Flutter"),
-          backgroundColor: const Color.fromARGB(255, 178, 3, 62),
-          foregroundColor: const Color.fromARGB(255, 196, 208, 26),
-          centerTitle: true,
-          elevation: 20.5,
-          shadowColor: Colors.blue,
-        ),
-        body: Row(children: [
-          Icon(
-            Icons.memory,
-            size: 60,
-            color: Colors.blue,
-
+          appBar: AppBar(
+            title: Text("Basic Flutter"),
+            backgroundColor: const Color.fromARGB(255, 178, 3, 62),
+            foregroundColor: const Color.fromARGB(255, 196, 208, 26),
+            centerTitle: true,
+            elevation: 20.5,
+            shadowColor: Colors.blue,
           ),
-          Text(
-            "Flutter Row",
-            style: TextStyle(
-              fontSize: 40,
-              color: Colors.red,
-            ),
-          ),
-
-        ],)
-      ),
+          body: Row(
+            children: [
+              Icon(
+                Icons.star,
+                size: 60,
+                color: const Color.fromARGB(255, 214, 228, 16),
+              ),
+               Icon(
+                Icons.star,
+                size: 60,
+                color: const Color.fromARGB(255, 214, 228, 16),
+              ),
+                Icon(
+                  Icons.star,
+                  size: 60,
+                  color: const Color.fromARGB(255, 180, 22, 22),
+                ),
+                Icon(
+                  Icons.star,
+                  size: 60,
+                  color: const Color.fromARGB(255, 173, 20, 20),
+                ),
+                Icon(
+                  Icons.star,
+                  size: 60,
+                  color: const Color.fromARGB(255, 190, 25, 25),
+                ),
+            ],
+          )),
     );
   }
 }
